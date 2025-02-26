e76a69447b4d907b55fb7f5187750f5dc5eedf78
Pe3et <wolfpeeet@googlemail.com>
2025-02-26 17:15:31 +0100

    desktop landingpage styling

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [FooterComponent],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.sass'
+})
+export class LandingPageComponent {
+
+}
