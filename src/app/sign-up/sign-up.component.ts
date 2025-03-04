cb3aefa372e56bc52170d57f38dc83290b7202b6
Pe3et <wolfpeeet@googlemail.com>
2025-03-04 15:18:59 +0100

    added JSDoc

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -17,7 +17,7 @@ export class SignUpComponent {
   isPasswordEmpty = false
   arePasswordsMatching = true
 
-  
+  /**Toggles the visibility of a password input.*/
   togglePasswordVisibility(field: 'password' | 'repeatedPassword') {
     if (field === 'password') {
       this.isPasswordVisible = !this.isPasswordVisible;
@@ -26,6 +26,7 @@ export class SignUpComponent {
     }
   }
 
+  /**Get's the correct svg depending on the visibiliyt of the password.*/
   getVisibilityIcon(field: 'password' | 'repeatedPassword') {
     if (field === 'password') {
       return this.isPasswordVisible
@@ -38,19 +39,23 @@ export class SignUpComponent {
     }
   }
 
+  /**Checks if the email is valid and handles the boolean for this validation.*/
   validateEmail(email: string) {
     const emailPattern = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
     this.isEmailValid = emailPattern.test(email)
   }
 
+  /**Checks if the password is given and handles the boolean for this validation.*/
   checkEmptyPassword(password: string) {
     this.isPasswordEmpty = password.length === 0
   }
 
+  /**Checks if the passwords match handles the boolean for this validation.*/
   validateRepeatedPassword(repeatedPassword: string, firstEnteredPassword: string) {
     this.arePasswordsMatching = repeatedPassword === firstEnteredPassword
   }
 
+  /**Registers the new user when everything is validated*/
   signUp(email: string, password: string, repeatedPassword: string){
     if(this.areAllInputFieldsValid(email, password, repeatedPassword)) {
       const postData = {
@@ -61,6 +66,7 @@ export class SignUpComponent {
     }
   }
 
+  /**Validates everything to make sure everything is correct for registration.*/
   areAllInputFieldsValid(email: string, password: string, repeatedPassword: string) {
     this.validateEmail(email);
     this.checkEmptyPassword(password);
