97c88a61574f501249ad2f37373808cacd8f0a2a
Pe3et <wolfpeeet@googlemail.com>
2025-03-11 17:29:14 +0100

    login component styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent},
@@ -11,4 +12,5 @@ export const routes: Routes = [
     { path: 'privacy-policy', component: PrivacyPolicyComponent },
     { path: 'sign-up', component: SignUpComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'forgot-password', component: ForgotPasswordComponent },
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FooterComponent, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.sass'
 })
 export class LoginComponent {
+  isPasswordVisible = false;
+  isPasswordEmpty = false;
+  isEmailValid = true;
+  sessionStorageMail = sessionStorage.getItem('email');
 
+  /**Handles the login request.*/
+  login(email: string, password: string) {
+    //TODO: login
+  }
+
+  /**Toggles the visibility of a password input.*/
+  togglePasswordVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  /**Get's the correct svg depending on the visibility of the password.*/
+  getVisibilityIcon() {
+    return this.isPasswordVisible
+      ? '/assets/img/visibility_off.svg'
+      : '/assets/img/visibility.svg'
+  }
+
+  /**Checks if the password is given and handles the boolean for this validation.*/
+  checkEmptyPassword(password: string) {
+    this.isPasswordEmpty = password.length === 0
+  }
+
+  /**Checks if the email is valid and handles the boolean for this validation.*/
+  validateEmail(email: string) {
+    const emailPattern = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    this.isEmailValid = emailPattern.test(email)
+  }
 }
