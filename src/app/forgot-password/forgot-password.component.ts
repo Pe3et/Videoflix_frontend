d8058b5db16d37997820564c9d9b0a762e723b1a
Pe3et <wolfpeeet@googlemail.com>
2025-03-13 16:14:59 +0100

    forgot pw component styling

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [CommonModule, FooterComponent, RouterLink],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.sass'
 })
 export class ForgotPasswordComponent {
+  isEmailValid = true;
+  sessionStorageMail = sessionStorage.getItem('email');
+
+  resetPassword(email: string){
+    
+  }
+
+  /**Checks if the email is valid and handles the boolean for this validation.*/
+  validateEmail(email: string) {
+    const emailPattern = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    this.isEmailValid = emailPattern.test(email)
+  }
 
 }
