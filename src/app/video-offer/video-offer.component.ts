5ba2f23bfd94f7c7d4f9cfc1d3fe5450199d9de0
Pe3et <wolfpeeet@googlemail.com>
2025-03-17 15:26:23 +0100

    responsive footer & add log out button icon

--- src/app/video-offer/video-offer.component.ts
+++ src/app/video-offer/video-offer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-video-offer',
-  imports: [FooterComponent, CommonModule],
+  imports: [FooterComponent, CommonModule, RouterLink],
   templateUrl: './video-offer.component.html',
   styleUrl: './video-offer.component.sass'
 })
