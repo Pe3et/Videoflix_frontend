5f934c7a8cc8350da4b0d6745b1600eaf93afd59
Pe3et <wolfpeeet@googlemail.com>
2025-03-17 15:38:53 +0100

    load auth token from session storage, redirect to login if there's no token

--- src/app/video-offer/video-offer.component.ts
+++ src/app/video-offer/video-offer.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-video-offer',
   imports: [FooterComponent, CommonModule, RouterLink],
   templateUrl: './video-offer.component.html',
-  styleUrl: './video-offer.component.sass'
+  styleUrls: ['./video-offer.component.sass']
 })
-export class VideoOfferComponent {
-  
+export class VideoOfferComponent implements OnInit {
+  token: string | null = null;
+
+  constructor(private router: Router) { }
+
+  /** Checks session storage for the auth token and stores it in a variable.
+   * If the token is found, it is used to populate the video offer component.
+   * If there is no token, there's redirection to login page.
+   */
+  ngOnInit(): void {
+    this.token = sessionStorage.getItem('token');
+    if (!this.token) {
+      this.router.navigate(['/login']);
+    } else {
+      //TODO: load videos/thumbails from DB
+    }
+  }
 }
